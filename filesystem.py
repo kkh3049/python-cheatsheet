89ba78b764e196dd3c5cd5d7f9ca068a25f2bb65
Keith Heiner <keith.heiner@gmail.com>
2022-08-17 09:20:00 -0700

    Update filesystem.py

--- filesystem.py
+++ filesystem.py
@@ -1,1 +1,10 @@
+def get_directory_from_filepath(filepath: str):
+  """ Path/to/file.txt => Path/to/ """
+  import os
+  return os.path.dirname(filepath)
 
+def get_filename_from_filepath(filepath: str):
+  """ Path/to/file.txt => file """
+  """ Path/to/file.tar.gz => file.tar """
+  from pathlib import Path
+  return Path(filepath).setm
